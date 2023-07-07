cdba5068fc3a5b7dbc47dfd0956b5f60df48652a
achaparro <achmarmol@gmail.com>
2023-07-06 20:25:29 -0400

    feat: agregado de componentes NavigaionHeader y Footer, completando la estructura basica de la web

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { MainRouter } from "./routers"
+import { NavigationHeader, Footer } from "./components"
 
 export const App = () => {
 
   return (
     <>
+      {/* Navigation */}
+      <NavigationHeader />
+
+      {/* Main Content */}
       <MainRouter />
+
+      {/* Footer */}
+      <Footer />
     </>
   )
 }
---
+++ src/components/NavigationHeader.tsx
@@ -0,0 +1,5 @@
+export const NavigationHeader = () => {
+  return (
+    <div>NavigationHeader</div>
+  )
+}
